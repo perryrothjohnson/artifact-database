c165ea7d424d84b56ece3b8d0b73257179d5ca56
Perry Roth-Johnson <prothjohnson@cscmail.org>
2017-06-14 15:46:31 -0700

    added more object attributes to pawtucket details page template

--- pawtucket/themes/csc/views/Details/ca_objects_default_html.php
+++ pawtucket/themes/csc/views/Details/ca_objects_default_html.php
@@ -26,9 +26,9 @@
  * ----------------------------------------------------------------------
  */
  
-	$t_object = 			$this->getVar("item");
-	$va_comments = 			$this->getVar("comments");
-	$va_tags = 				$this->getVar("tags_array");
+	$t_object = 		$this->getVar("item");
+	$va_comments = 		$this->getVar("comments");
+	$va_tags = 		$this->getVar("tags_array");
 	$vn_comments_enabled = 	$this->getVar("commentsEnabled");
 	$vn_share_enabled = 	$this->getVar("shareEnabled");
 
@@ -74,89 +74,28 @@
 			<div class='col-sm-6 col-md-6 col-lg-5'>
 				<!-- collection name, object name -->
 				<H4>{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</H4>
-
-<!--
-				<H6>{{{<unit>^ca_objects.type_id</unit>}}}</H6>
--->
 				
-				<!-- accession number -->
-				{{{<ifdef code="ca_objects.idno"><HR><H6>Accession number:</H6>^ca_objects.idno<br/></ifdef>}}}
+				<br/><HR><H5>*********************** OBJECT INFO ***********************</H5>
 
-				<!-- previous ID numbers -->
-				{{{<ifdef code="ca_objects.altID"><HR><H6>Previous ID numbers:</H6>
-					<span class="trimText">
-						<unit delimiter="<br/>">^ca_objects.altID.previous_ID_num (^ca_objects.altID.description_prev_ID_num)</unit>
-					</span>
-				</ifdef>}}}
+				<!-- supergroup -->
+				{{{<ifdef code="ca_objects.supergroup"><HR><H6>Supergroup:</H6>^ca_objects.supergroup<br/></ifdef>}}}
 
-				<!-- Phase III exhibit code -->
-				{{{<ifdef code="ca_objects.phase_3_exhibit_code"><HR><H6>Phase III exhibit code:</H6>^ca_objects.phase_3_exhibit_code<br/></ifdef>}}}
+				<!-- group -->
+				{{{<ifdef code="ca_objects.group"><HR><H6>Group:</H6>^ca_objects.group<br/></ifdef>}}}
+
+				<!-- quantity -->
+				{{{<ifdef code="ca_objects.extent"><HR><H6>Quantity:</H6>^ca_objects.extent<br/></ifdef>}}}
 
 				<!-- administrative division -->
-				{{{<ifdef code="ca_objects.administrative_division"><HR><H6>Administrative division:</H6>^ca_objects.administrative_division<br/></ifdef>}}}
-
+				<!--{{{<ifdef code="ca_objects.administrative_division"><HR><H6>Administrative division:</H6>^ca_objects.administrative_division<br/></ifdef>}}}-->
 				<!-- category -->
-				{{{<ifdef code="ca_objects.object_category"><HR><H6>Category:</H6>^ca_objects.object_category<br/></ifdef>}}}
+				<!--{{{<ifdef code="ca_objects.object_category"><HR><H6>Category:</H6>^ca_objects.object_category<br/></ifdef>}}}-->
 				<!-- classification -->
-				{{{<ifdef code="ca_objects.item_classification"><H6>Classification:</H6>^ca_objects.item_classification<br/></ifdef>}}}
+				<!--{{{<ifdef code="ca_objects.item_classification"><HR><H6>Classification:</H6>^ca_objects.item_classification<br/></ifdef>}}}-->
 
 				<!-- alternate object names -->
 				{{{<ifdef code="ca_objects.alternate_object_names"><HR><H6>Alternate object names:</H6>
 					<unit delimiter="<br/>">^ca_objects.alternate_object_names</unit>
-				</ifdef>}}}
-
-				<!-- date of manufacture -->
-				{{{<ifdef code="ca_objects.date_manufacture"><HR><H6>Date of manufacture:</H6>^ca_objects.date_manufacture<br/></ifdef>}}}
-
-				<!-- acquisition method -->
-				{{{<ifdef code="ca_objects.acquisition_type_id"><HR><H6>Acquisition method:</H6>^ca_objects.acquisition_type_id<br/></ifdef>}}}
-
-				<!-- donor -->
-				{{{<ifdef code="ca_objects.donor"><HR><H6>Donor/Lender/Vendor/Transfer party:</H6>
-					<unit delimiter="<br/>">^ca_objects.donor</unit>
-				</ifdef>}}}
-				
-				<!-- donor attribution -->
-				{{{<ifdef code="ca_objects.donor_attribution"><HR><H6>Donor attribution:</H6>
-					<unit delimiter="<br/>">^ca_objects.donor_attribution</unit>
-				</ifdef>}}}
-
-				<!-- loan info -->
-				{{{<ifdef code="ca_loans.preferred_labels"><HR><H6>Loan info:</H6>^ca_loans.preferred_labels (loan #^ca_loans.idno)<br/></ifdef>}}}
-
-				<!-- object function -->
-				{{{<ifdef code="ca_objects.object_function"><HR><H6>Object function:</H6>
-					<span class="trimText">^ca_objects.object_function</span>
-				</ifdef>}}}
-
-
-				<!-- object description -->
-				{{{<ifdef code="ca_objects.description"><HR><H6>Object description:</H6>
-					<span class="trimText">^ca_objects.description</span>
-				</ifdef>}}}
-
-				<!-- storage locations -->
-				{{{<ifdef code="ca_storage_locations.hierarchy.preferred_labels.name"><HR><H6>Storage/museum location:</H6>
-					<unit delimiter="&nbsp;&#10141;&nbsp;">^ca_storage_locations.hierarchy.preferred_labels.name</unit><br/>
-				</ifdef>}}}
-
-				<!-- dimensions -->
-				{{{<ifdef code="ca_objects.length_dimension"><HR><H6>Length (or diameter):</H6>^ca_objects.length_dimension<br/></ifdef>}}}
-				{{{<ifdef code="ca_objects.width_dimension"><HR><H6>Width:</H6>^ca_objects.width_dimension<br/></ifdef>}}}
-				{{{<ifdef code="ca_objects.height_dimension"><HR><H6>Height (or thickness):</H6>^ca_objects.height_dimension<br/></ifdef>}}}
-				{{{<ifdef code="ca_objects.weight_dimension"><HR><H6>Weight:</H6>^ca_objects.weight_dimension<br/></ifdef>}}}
-
-				<!-- object materials -->
-				{{{<ifdef code="ca_objects.format_text"><HR><H6>Object materials:</H6>
-					<unit delimiter=", "<l>^ca_objects.format_text</l></unit>
-				</ifdef>}}}
-
-				<!-- condition -->
-				{{{<ifdef code="ca_objects.condition"><HR><H6>Condition:</H6>^ca_objects.condition<br/></ifdef>}}}
-
-				<!-- handling and maintenance -->
-				{{{<ifdef code="ca_objects.handling_and_maintenance"><HR><H6>Special handling and maintenance instructions:</H6>
-					<span class="trimText">^ca_objects.handling_and_maintenance</span>
 				</ifdef>}}}
 
 				<!-- notes -->
@@ -164,7 +103,7 @@
 					<span class="trimText">^ca_objects.internal_notes</span>
 				</ifdef>}}}
 
-				<!-- documentation info (from this object) -->
+				<!-- documentation info (for this object) -->
 				{{{<ifdef code="ca_objects.documentation_info"><HR><H6>Documentation info (for this object):</H6>
 					<span class="trimText">
 					<?php
@@ -182,6 +121,97 @@
 					?>
 					</span>
 				</ifdef>}}}
+
+
+				
+				<br/><HR><H5>*********************** ID NUMBERS ***********************</H5>
+
+				<!-- object ID/accession number -->
+				{{{<ifdef code="ca_objects.idno"><HR><H6>Object ID number:</H6>^ca_objects.idno<br/></ifdef>}}}
+				<!-- accessioned? -->
+				{{{<ifdef code="ca_objects.accessioned"><HR><H6>Accessioned?:</H6>^ca_objects.accessioned<br/></ifdef>}}}
+				
+				<!-- Phase III exhibit code -->
+				{{{<ifdef code="ca_objects.phase_3_exhibit_code"><HR><H6>Phase III exhibit code:</H6>^ca_objects.phase_3_exhibit_code<br/></ifdef>}}}
+				<!-- part number -->
+				{{{<ifdef code="ca_objects.part_number"><HR><H6>Part number:</H6>^ca_objects.part_number<br/></ifdef>}}}
+				<!-- serial number -->
+				{{{<ifdef code="ca_objects.serial_number"><HR><H6>Serial number:</H6>^ca_objects.serial_number<br/></ifdef>}}}
+				<!-- previous ID numbers -->
+				{{{<ifdef code="ca_objects.altID"><HR><H6>Previous ID numbers:</H6>
+					<span class="trimText">
+						<unit delimiter="<br/>">^ca_objects.altID.previous_ID_num (^ca_objects.altID.description_prev_ID_num)</unit>
+					</span>
+				</ifdef>}}}
+
+				<!-- ITAR status -->
+				{{{<ifdef code="ca_objects.itar_status"><HR><H6>ITAR status:</H6>^ca_objects.itar_status<br/></ifdef>}}}
+				<!-- ITAR code -->
+				{{{<ifdef code="ca_objects.itar_code"><HR><H6>ITAR code:</H6>^ca_objects.itar_code<br/></ifdef>}}}
+
+
+
+				<br/><HR><H5>******************** LOCATION ********************</H5>
+				
+				<!-- storage locations -->
+				{{{<ifdef code="ca_storage_locations.hierarchy.preferred_labels.name"><HR><H6>Storage/museum location:</H6>
+					<unit delimiter="&nbsp;&#10141;&nbsp;">^ca_storage_locations.hierarchy.preferred_labels.name</unit><br/>
+				</ifdef>}}}
+
+
+
+				<br/><HR><H5>******************** DIMENSIONS ********************</H5>
+
+				<!-- dimensions -->
+				{{{<ifdef code="ca_objects.length_dimension"><HR><H6>Length (or diameter):</H6>^ca_objects.length_dimension<br/></ifdef>}}}
+				{{{<ifdef code="ca_objects.width_dimension"><H6>Width:</H6>^ca_objects.width_dimension<br/></ifdef>}}}
+				{{{<ifdef code="ca_objects.height_dimension"><H6>Height (or thickness):</H6>^ca_objects.height_dimension<br/></ifdef>}}}
+				{{{<ifdef code="ca_objects.weight_dimension"><H6>Weight:</H6>^ca_objects.weight_dimension<br/></ifdef>}}}
+
+
+
+				<HR><H5>******************** ACQUISITION INFO ********************</H5>
+
+				<!-- donor/related entities -->
+				{{{<ifdef code="ca_entities.preferred_labels.displayname"><HR><H6>Donor/Lender/Vendor/Transfer party:</H6>
+					<?php
+						$names = $t_object->get(
+							'ca_entities.preferred_labels.displayname', 
+							array("returnAsArray"=>true, "restrictToRelationshipTypes"=>array("donor", "lender", "vendor", "trader", "transfer_party")));
+						$arrlength = count($names);
+						for($x=0; $x<$arrlength; $x++) {
+							echo $names[$x];
+							echo "<br/>";
+						}
+					?>
+				</ifdef>}}}
+				<!-- donor attribution -->
+				{{{<ifdef code="ca_objects.donor_attribution"><H6>Donor attribution:</H6>
+					<unit delimiter="<br/>">^ca_objects.donor_attribution.donor_attrib_text</unit>
+				</ifdef>}}}
+
+				<!-- acquisition method -->
+				{{{<ifdef code="ca_object_lots.type_id"><HR><H6>Acquisition method:</H6>^ca_object_lots.type_id<br/></ifdef>}}}
+				<!-- object lot ID -->
+				{{{<ifdef code="ca_object_lots.preferred_labels"><HR><H6>Object lot ID:</H6>^ca_object_lots.preferred_labels (^ca_object_lots.idno_stub)<br/></ifdef>}}}
+				<!-- DGS paid -->
+				{{{<ifdef code="ca_object_lots.DGS_paid_lot"><HR><H6>Acquisition method:</H6>^ca_object_lots.DGS_paid_lot<br/></ifdef>}}}
+				<!-- loan info -->
+				{{{<ifdef code="ca_loans.preferred_labels"><HR><H6>Loan info:</H6>^ca_loans.preferred_labels (loan #^ca_loans.idno)<br/></ifdef>}}}
+
+				<!-- transfer date -->
+				{{{<ifdef code="ca_objects.transfer_date"><HR><H6>Transfer date:</H6>^ca_objects.transfer_date<br/></ifdef>}}}
+				<!-- delivery date -->
+				{{{<ifdef code="ca_objects.delivery_date"><HR><H6>Delivery date:</H6>^ca_objects.delivery_date<br/></ifdef>}}}
+
+				<!-- owned by/loaned to -->
+				{{{<ifdef code="ca_objects.owned_by_loaned_to"><HR><H6>Owned by/Loaned to:</H6>^ca_objects.owned_by_loaned_to<br/></ifdef>}}}
+				<!-- item value -->
+				{{{<ifdef code="ca_objects.item_value"><HR><H6>Object value:</H6>^ca_objects.item_value<br/></ifdef>}}}
+				<!-- insurance value -->
+				{{{<ifdef code="ca_objects.insurance_value"><HR><H6>Insurance value:</H6>^ca_objects.insurance_value<br/></ifdef>}}}
+				<!-- insurance method -->
+				{{{<ifdef code="ca_objects.insurance_method"><HR><H6>Insurance method:</H6>^ca_objects.insurance_method<br/></ifdef>}}}
 
 				<!-- documentation info (from object lot) -->
 				{{{<ifdef code="ca_object_lots.documentation_info"><HR><H6>Documentation info (from object lot #^ca_object_lots.idno_stub):</H6>
@@ -221,6 +251,44 @@
 					</span>
 				</ifdef>}}}
 
+
+
+				<HR><H5>******************** CONSERVATION ********************</H5>
+
+				<!-- condition -->
+				{{{<ifdef code="ca_objects.condition"><HR><H6>Condition:</H6>^ca_objects.condition<br/></ifdef>}}}
+
+				<!-- object materials -->
+				{{{<ifdef code="ca_objects.format_text"><HR><H6>Object materials:</H6>
+					<unit delimiter=", "<l>^ca_objects.format_text</l></unit>
+				</ifdef>}}}
+
+				<!-- handling and maintenance -->
+				{{{<ifdef code="ca_objects.handling_and_maintenance"><HR><H6>Special handling and maintenance instructions:</H6>
+					<span class="trimText">^ca_objects.handling_and_maintenance</span>
+				</ifdef>}}}
+
+
+
+				<HR><H5>******************** RESEARCH ********************</H5>
+
+				<!-- object function -->
+				{{{<ifdef code="ca_objects.object_function"><HR><H6>Object function:</H6>
+					<span class="trimText">^ca_objects.object_function</span>
+				</ifdef>}}}
+
+				<!-- object description -->
+				{{{<ifdef code="ca_objects.description"><HR><H6>Object description:</H6>
+					<span class="trimText">^ca_objects.description</span>
+				</ifdef>}}}
+
+				<!-- date of manufacture -->
+				{{{<ifdef code="ca_objects.date_manufacture"><HR><H6>Date of manufacture:</H6>^ca_objects.date_manufacture<br/></ifdef>}}}
+
+
+
+				<HR><H5>******************** RELATED PEOPLE AND OBJECTS ********************</H5>
+
 				<!-- related people, places, and objects -->
 				<hr></hr>
 					<div class="row">
@@ -229,10 +297,11 @@
 							{{{<ifcount code="ca_entities" min="2"><H6>Related people</H6></ifcount>}}}
 							{{{<unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit>}}}
 							
-							
+							<!--
 							{{{<ifcount code="ca_places" min="1" max="1"><H6>Related place</H6></ifcount>}}}
 							{{{<ifcount code="ca_places" min="2"><H6>Related places</H6></ifcount>}}}
 							{{{<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit>}}}
+							-->
 							
 							{{{<ifcount code="ca_list_items" min="1" max="1"><H6>Related Term</H6></ifcount>}}}
 							{{{<ifcount code="ca_list_items" min="2"><H6>Related Terms</H6></ifcount>}}}
